13088f479b85cd5d8281232d20e46391657c1d69
Hudi <hudi1989@gmail.com>
2017-01-27 22:13:23 +0100

    gradle config

---
+++ test/main/org/eit/FirstTests.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import org.eit.*;
+
+public class FirstTests {
+
+    @Test
+    public void multiplicationOfZeroIntegersShouldReturnZero() {
+        assertEquals("HelloWorld::asd() must be 12",12, Main.asd());
+    }
+}
