9ea53715f843eb1bcb51c0f1096d33ca06b97190
Hudi <hudi1989@gmail.com>
2017-03-28 16:54:27 +0200

    removed comment

--- example4/src/org/eit/Main.java
+++ example4/src/org/eit/Main.java
@@ -3,7 +3,6 @@ package org.eit;
 public class Main {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
     
