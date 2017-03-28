7d4240732745c07605f7c857963450f29db86494
Hudi <hudi1989@gmail.com>
2017-03-28 16:38:39 +0200

    changes

--- example2/src/org/eit/Main.java
+++ example2/src/org/eit/Main.java
@@ -3,13 +3,7 @@ package org.eit;
 public class Main {
 
     public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
     }
-    
-    public static int asd(){
-        return 12;
-    }
-
 }
 
