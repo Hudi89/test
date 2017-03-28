b85b665c68356f29419126b082ac64bb42a994b6
Hudi <hudi1989@gmail.com>
2017-03-28 15:29:52 +0200

    add example state 2-3

---
+++ example3/src/main/org/eit/Main.java
@@ -0,0 +1,15 @@
+package org.eit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
+    }
+    
+    public static int asd(){
+        return 12;
+    }
+
+}
+
