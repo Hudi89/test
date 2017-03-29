aeeee8a6b5eb0f395a1a30e07d83ce07260317d0
Hudoba Peter <hupqaai@INF.ELTE.HU>
2017-03-29 17:51:21 +0200

    add idea project

---
+++ c2/src/org/eit/Main.java
@@ -0,0 +1,13 @@
+package org.eit;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+
+    public static int asd(){
+        return 132;
+    }
+
+}
